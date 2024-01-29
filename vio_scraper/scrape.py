16bb0ee7cba38215d89d51e74e0712dd1cadb026
Eric Beaulne <eric.minecraft20@gmail.com>
2024-01-29 14:42:41 -0500

    the changes

--- vio_scraper/scrape.py
+++ vio_scraper/scrape.py
@@ -5,7 +5,8 @@ import pyautogui
 import numpy as np
 import multiprocessing
 from PIL import ImageGrab, Image, ImageDraw
-from .extraction import TableExtraction
+# from .extraction import TableExtraction
+from .new_extraction import ImageProcessing
 from datetime import datetime, timezone
 
 logger = logging.getLogger("LucaScraper")
@@ -17,14 +18,12 @@ class ItemNotFound(Exception):
 # Config                   #
 ############################
 
-def worker_init(path: str | None):
-    global table
-    table = TableExtraction(
-        rec_model_dir=path,
-    )
-
 def worker(data):
-    return table.extract_table(data)
+    img = ImageProcessing()
+    return {
+        "name": data["name"],
+        "data": img.extract_data_from_image(data["image"], data["extra_buy"], data["extra_sell"])
+    }
 
 class ItemScraper:
 
@@ -43,7 +42,7 @@ class ItemScraper:
             "items": {}
         }
         cpus = multiprocessing.cpu_count()
-        with multiprocessing.Pool(min(cpus, len(items)), initializer=worker_init, initargs=(self.model_path,)) as p:
+        with multiprocessing.Pool(min(cpus, len(items))) as p:
             images = []
             for item in items:
                 logger.debug("Scraping item: " + item)
@@ -59,11 +58,13 @@ class ItemScraper:
                             if x > 10:
                                 raise ItemNotFound(f"{item} not found")
                         self.waitForItemLoad()
-                        item_scan = self.improvedScanItem()
+                        item_scan, extra_sells, extra_buys = self.improvedScanItem()
                         self.closeItem()
                         images.append(
                             {
                                 "name": item,
+                                "extra_sell": extra_sells,
+                                "extra_buy": extra_buys,
                                 "image" : item_scan
                             }
                         )
@@ -113,20 +114,10 @@ class ItemScraper:
             top = original[:split_point, :]
             bottom = original[split_point:, :]
             original = np.concatenate((top, sells, bottom), axis=0)
-
-        height = original.shape[0]
-        original_pil = Image.fromarray(original)
-        draw = ImageDraw.Draw(original_pil)
-        height = original_pil.height
-        for x in [76, 210]:
-            draw.line([(x, 0), (x, height)], fill=(0, 0, 0), width=5)
-
-        original = np.array(original_pil)
-
         return original
 
     def improvedScanItem(self) -> np.ndarray:
-        inital_screenshot = self.take_screenshot_of_region("item")
+        initial_screenshot = self.take_screenshot_of_region("item")
         sell_screenshot, buy_screenshot = None, None
         if self.get_color_at_pixel(self.config["sell_wheel"]) == (143, 143, 143):
             while self.get_color_at_pixel(self.config["sell_wheel"]) == (143, 143, 143):
@@ -139,11 +130,11 @@ class ItemScraper:
                 pydirectinput.dragRel(None, 180, 0.5, button="left")
             buy_screenshot = self.take_screenshot_of_region("buy")
         final_screenshot = self.merge_screenshot(
-            inital_screenshot, 
+            initial_screenshot, 
             sell_screenshot, 
             buy_screenshot
         )
-        return final_screenshot
+        return final_screenshot, sell_screenshot is not None, buy_screenshot is not None
     
     def extract_listings(self, data: list[tuple[str, str, str]]):
         listings = data[5:]
