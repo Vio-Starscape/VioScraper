845d58bf270f83cf09265a04c7708f10220b084d
Eric Beaulne <eric.minecraft20@gmail.com>
2024-05-02 20:15:16 -0400

    fix: implemented ore checking and auto starting

--- vio_scraper/ram.py
+++ vio_scraper/ram.py
@@ -18,29 +18,32 @@ class RAM:
         self.current_account: dict | None = None
 
     def __enter__(self):
+        if "Roblox Account Manager.exe" not in [proc.name() for proc in psutil.process_iter()]:
+            print("Launching Roblox Account Manager")
+            psutil.Popen([r"C:\Users\ericm\Desktop\Roblox Account Manager\Roblox Account Manager.exe"])
+            time.sleep(10)
+
+        print("Updating here...")
+        self.update_here()
         while self.login_sequence():
             pass
         return self
     
     def __exit__(self, exc_type, exc_value, exc_traceback):
         self.exit_roblox()
+        self.update_there()
 
     def update_here(self):
         r = requests.get(f'{self.uri}/api/scraper/getall',
                          headers={"x-api-key": self.apikey})
-        print(r.status_code)
-        print(r.content)
         response_data = r.json()
 
         current_accounts = self.get_accounts_json()
 
         for account in response_data:
-            print(account)
             if current_accounts[account["name"]] == "yoinked" and not account["yoinked"]:
-                print("here")
                 self.unmark_yoinked_account(account["name"])
             elif current_accounts[account["name"]] != "yoinked" and account["yoinked"]:
-                print("Here2")
                 self.mark_account_as_yoinked(account["name"])
         
         print("Update Done!")
@@ -50,7 +53,7 @@ class RAM:
         for name, description in self.get_accounts_json().items():
             account_data.append({
                 "name": name,
-                "active": self.current_account is not None and name in self.current_account.keys(),
+                "active": self.current_account is not None and name in self.current_account,
                 "yoinked": "yoinked" in description
             })
         r = requests.post(
@@ -58,8 +61,6 @@ class RAM:
             headers={"x-api-key": self.apikey},
             json=account_data
         )
-        print(r.status_code)
-        print(r.content)
 
     def jiggle_mouse(self):
         pydirectinput.moveRel(1, 1)
@@ -67,8 +68,6 @@ class RAM:
         pydirectinput.moveRel(-1, -1)
     
     def login_sequence(self):
-
-        self.update_here()
 
         accounts = self.get_non_yoinked_accounts()
         if len(accounts) == 0:
@@ -78,8 +77,6 @@ class RAM:
 
         print("Using account: ", self.current_account)
 
-        self.update_there()
-
         def wait_for(func, *args, func2 = None):
             initial = time.perf_counter()
             while not func(*args):
@@ -88,24 +85,28 @@ class RAM:
                 if time.perf_counter() - initial > 20:
                     return False
             return True
-        wait_for(
+        if not wait_for(
             lambda x, y: ImageGrab.grab().getpixel((x, y)) == tuple(self.config["starscape_color"]), 
             *self.config["starscape_logo"],
             func2=self.bring_to_front
-        )
+        ):
+            return False
         print("Found logo")
         pydirectinput.moveTo(*self.config["starscape_button"])
         pydirectinput.click()
         self.jiggle_mouse()
         time.sleep(1)
         pydirectinput.click()
-        wait_for(
+        if not wait_for(
             lambda x, y: ImageGrab.grab().getpixel((x, y)) == tuple(self.config["starscape_health_color"]), 
             *self.config["starscape_health"]
-        )
+        ):
+            return False
         time.sleep(2)
         pydirectinput.press("f")
         time.sleep(5)
+
+        return False
 
     def exit_roblox(self):
         for proc in psutil.process_iter():
@@ -129,8 +130,6 @@ class RAM:
             r = requests.get(f'{self.ramuri}/LaunchAccount?Account={account}&PlaceId=679715583&Password={self.password}', timeout=1)
         except requests.exceptions.ReadTimeout:
             pass
-
-        self.update_there()
         
     def bring_to_front(self):
         try:
